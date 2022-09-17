d2f609dfb27778fe443178b8ee20828521787fcf
imsourobh <100500795+imsourobh@users.noreply.github.com>
2022-09-17 06:57:37 +0600

    Update dec to bin.c

--- dec to bin.c
+++ dec to bin.c
@@ -3,11 +3,12 @@
 int main()
 {
     int n=0,dec,bin=0;
+    printf("Enter decimal number: ");
     scanf("%d",&dec);
     
-    printf("Binary equivalent of %d is :",dec);
+    printf("\nBinary equivalent of %d is :",dec);
     do{
-      bin=(dec%2)*pow(10,n);
+      bin+=(dec%2)*pow(10,n);
       n++;
       dec=dec/2;
     }while(dec!=0);
